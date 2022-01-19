3d2176c0643c53552ff0408f254a3b90d212b4cf
Sharon Odima <odimasharon@gmail.com>
2022-01-19 05:28:09 -0500

    Sharon Odima Completed module3Week2 Project

--- index.js
+++ index.js
@@ -39,9 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(edible) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(edible);
+  }
+}
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
 
 
 
@@ -63,10 +80,28 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
+Car.prototype.fill = function(gallons) {
+  this.tank = this.tank + gallons
+}
+
+Car.prototype.drive = function(distance) {
+  const driveableMiles = this.tank * this.milesPerGallon;
+  if (dist <= driveableMiles) {
+    this.odometer = this.odometer + dist;
+    this.tank = this.tank -(dist/this.milesPergallon);
+  } else {
+    this.odometer = this.odometer + driveableMiles;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles`
+  }
+}
 
 /*
   TASK 3
@@ -75,18 +110,23 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
 
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function() {
+  return `Playing withb${this.favoriteToy}`;
+}
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Windoew/ Global Object Binding - when none of the other rules apply, this will reurn the window or global object in node or undefied in strict mode.
+  2. Implicit Binding - Whenever a preceding dot calls a function, the object before the dot is this.
+  3. New Binding - Whenever we use a constructor function, this refers to the specific instance of the object that is created and returned by the constructor function.
+  4. Explicit Binding - Whenever we use JavaScript's call or apply method, this is explicitly defined.
 */
 
 
